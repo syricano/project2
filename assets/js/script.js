6cf3a36ab082565d5398991bf978356d0081c60a
Anass Muhammad Ali <syriacano@gmail.com>
2023-09-16 01:24:37 +0200

    declare constants & possible choices to javasciprt file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Declaring constants & possible choices
+
+var choices = ["rock", "paper", "scissors"];
+var result = document.getElementById("result");
+var playerSelection = document.getElementById("p-display");
+var computerSelection = document.getElementById("c-display");
+var playerScore = 0; // Initialize player score
+var computerScore = 0; // Initialize computer score
