a62427b93a5d59665a3aad5f8b2bd965f4ed02ee
Anass Muhammad Ali <syriacano@gmail.com>
2023-09-16 01:25:54 +0200

    add event listener to images to get player choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ var playerSelection = document.getElementById("p-display");
 var computerSelection = document.getElementById("c-display");
 var playerScore = 0; // Initialize player score
 var computerScore = 0; // Initialize computer score
+
+// Add event listeners to the set player choices:
+
+document.getElementById("rock").addEventListener("click", function() {
+    setPlayerChoice("rock");
+});
+document.getElementById("paper").addEventListener("click", function() {
+    setPlayerChoice("paper");
+});
+document.getElementById("scissor").addEventListener("click", function() {
+    setPlayerChoice("scissors");
+});
