57579f2f94019f5ddf271d947ac24bd4fc0ab763
Anass Muhammad Ali <syriacano@gmail.com>
2023-11-17 21:47:34 +0100

    add aria label to index and json

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,13 @@ const rulesBtn = document.querySelector(".rules-button");
 
 // Add event listeners to the set player choices:
 
-document.getElementById("rock").addEventListener("click", function() {
+document.getElementById("rock").addEventListener("click", function () {
     setPlayerChoice("rock");
 });
-document.getElementById("paper").addEventListener("click", function() {
+document.getElementById("paper").addEventListener("click", function () {
     setPlayerChoice("paper");
 });
-document.getElementById("scissor").addEventListener("click", function() {
+document.getElementById("scissor").addEventListener("click", function () {
     setPlayerChoice("scissors");
 });
 /** Set player Choice function */
@@ -35,50 +35,52 @@ function setComputerChoice() {
 }
 
 /** Main Run game function */
-function runGame(){
+function runGame() {
     // Set computer choice before comparing
- var computerChoice = setComputerChoice();
-/** Cases */
-// Case 1: 
-if (playerChoice === computerChoice) {
-    result.innerHTML = "Same same";
-} else if (
-// Case 2:
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
-) {
-    result.innerHTML = "You Win";
-    playerScore++; // Increment player's score
-    document.getElementById("player-score").innerHTML = playerScore; // Update player's score display
-} else {
-// Case 3    
-    result.innerHTML = "Computer Wins";
-    computerScore++; // Increment computer's score
-    document.getElementById("computer-score").innerHTML = computerScore; // Update computer's score display
+    var computerChoice = setComputerChoice();
+    /** Cases */
+    // Case 1: 
+    if (playerChoice === computerChoice) {
+        result.innerHTML = "Same same";
+    } else if (
+        // Case 2:
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result.innerHTML = "You Win";
+        playerScore++; // Increment player's score
+        document.getElementById("player-score").innerHTML = playerScore; // Update player's score display
+    } else {
+        // Case 3    
+        result.innerHTML = "Computer Wins";
+        computerScore++; // Increment computer's score
+        document.getElementById("computer-score").innerHTML = computerScore; // Update computer's score display
+    }
+
+    // Update result with aria-live region
+    result.setAttribute("aria-live", "assertive");
+    result.setAttribute("aria-label", roundResult);
 }
 
+/** Set Share button */
+var shareV = document.getElementById("share");
+shareV.addEventListener("click", shareMe);
 
+function shareMe() {
+    var shareIt = {
+        text: "I'm playing a Rock, Paper, Scissor Game",
+        url: window.location.href
+    };
+
+    if (navigator.share) {
+        navigator.share(shareIt);
+
+    } else {
+        // Fallback for browsers that don't support the Web Share API
+        alert("Sorry, your browser does not support sharing. You can manually copy and share the URL.");
+    }
 }
-
-  /** Set Share button */
-  var shareV=document.getElementById("share");
-  shareV.addEventListener("click", shareMe);
-
-  function shareMe() {
-      var shareIt = {
-          text: "I'm playing a Rock, Paper, Scissor Game",
-          url: window.location.href
-      };
-  
-      if (navigator.share) {
-          navigator.share(shareIt);
-            
-      } else {
-          // Fallback for browsers that don't support the Web Share API
-          alert("Sorry, your browser does not support sharing. You can manually copy and share the URL.");
-      }
-  }
 // function to show/disappear rules span  
 function rulesOnOff() {
     var popup = document.getElementById("rules-spaner");
